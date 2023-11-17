0c7ba21b7210e75a4c1d02acc26429b4e6ac47bf
chtah <chawakorn_ha@hotmail.com>
2023-11-17 21:05:03 +0700

    initail setup

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom'
+import classes from './Navbar.module.css'
+import { useAuth } from '../providers/AuthProvider'
+import { NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+  const { isLoggedIn, logout } = useAuth()
+  return (
+    <div className={classes.nav}>
+      <Link className={classes.logo} to={'/'}>
+        <p className={classes.logoName}>LIVEY</p>
+      </Link>
+
+      <div className={classes.navRight}>
+        {isLoggedIn ? (
+          <>
+            <NavLink className={({ isActive }) => (isActive ? classes.active : classes.inActive)} to="/profile">
+              Journal
+            </NavLink>
+
+            <p className={classes.inActive} onClick={logout}>
+              Logout
+            </p>
+          </>
+        ) : (
+          <NavLink className={({ isActive }) => (isActive ? classes.active : classes.inActive)} to="/login">
+            Login
+          </NavLink>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import classes from './Home.module.css'
+const Home = () => {
+  return (
+    <div>
+      <h1 className={classes.title}>Welcome Starter</h1>
+    </div>
+  )
+}
+
+export default Home
