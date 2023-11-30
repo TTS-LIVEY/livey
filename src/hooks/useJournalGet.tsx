65603d3c470a496c40ffd875036b27ed222b0884
chtah <chawakorn_ha@hotmail.com>
2023-11-30 11:27:36 +0700

    update journal

--- src/hooks/useJournalGet.tsx
+++ src/hooks/useJournalGet.tsx
@@ -6,18 +6,20 @@ import { API_HOST } from '../utils/url'
 const useJournalGet = () => {
   const [journalGet, setJournalGet] = useState<JournalDTO[] | null>(null)
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await axios.get<JournalDTO[]>(`${API_HOST}/journal/`)
-        setJournalGet(res.data)
-      } catch (err) {
-        console.error(err)
-      }
+  const fetchData = async () => {
+    try {
+      const res = await axios.get<JournalDTO[]>(`${API_HOST}/journal/`)
+      setJournalGet(res.data)
+    } catch (err) {
+      console.error(err)
     }
+  }
+
+  useEffect(() => {
     fetchData()
   }, [])
 
-  return { journalGet }
+  return { journalGet, fetchData }
 }
+
 export default useJournalGet
