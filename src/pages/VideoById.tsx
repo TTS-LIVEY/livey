48a083631a5193021f3df63a859bf1da8df2ae97
chtah <chawakorn_ha@hotmail.com>
2023-11-30 16:35:04 +0700

    favorite proof

--- src/pages/VideoById.tsx
+++ src/pages/VideoById.tsx
@@ -5,13 +5,61 @@ import ReactPlayer from 'react-player'
 import Loading from '../components/Loading'
 import useHistoryCreate from '../hooks/useHistoryCreate'
 import Navbar from '../components/Navbar/Navbar'
-
+import IconButton from '@mui/joy/IconButton'
+import FavoriteBorder from '@mui/icons-material/FavoriteBorder'
+import Favorite from '@mui/icons-material/Favorite'
+import { useEffect, useState } from 'react'
+import useHistoryGet from '../hooks/useHistoryGet'
+import axios from 'axios'
+import { API_HOST } from '../utils/url'
 const VideoById = () => {
+  const token = localStorage.getItem('token')
   const { id } = useParams()
   const { videoById, isError, isLoading } = useVideoGetById(id || '1')
   const navigate = useNavigate()
+  const [isFavorite, setIsFavorite] = useState<boolean>(false)
 
   useHistoryCreate(id || '1')
+
+  const { newHistory } = useHistoryGet()
+  const historyId = newHistory?.find((data) => {
+    return data.contentId === Number(id)
+  })
+  console.log(historyId)
+
+  useEffect(() => {
+    if (historyId !== undefined) {
+      setIsFavorite(historyId.is_favorite)
+    }
+  }, [historyId])
+
+  const favoriteHandler = async () => {
+    try {
+      await axios.patch(
+        `${API_HOST}/history/fav/${historyId?.history_id}`,
+        { is_favorite: !historyId?.is_favorite },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      )
+
+      window.location.reload()
+
+      const updatedHistory = await axios.get(`${API_HOST}/history/${historyId?.history_id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+
+      // Update state with the correct is_favorite status
+      setIsFavorite(updatedHistory.data.is_favorite)
+    } catch (error) {
+      console.error('Error updating favorite status:', error)
+    }
+  }
 
   if (isLoading || !videoById) return <Loading />
 
@@ -26,6 +74,15 @@ const VideoById = () => {
         <div className={`${classes.Tags} flex gap-6 mt-5 mb-3 text-xl`}>
           <div className="rounded-3xl border-black border bg-[#f3f3f3] py-2 px-4">{videoById.video_type}</div>
           <div className="rounded-3xl border-black border bg-[#f3f3f3] py-2 px-4">{videoById.body_part}</div>
+          {isFavorite ? (
+            <IconButton color="danger" onClick={favoriteHandler} size="lg" variant="plain">
+              <Favorite />
+            </IconButton>
+          ) : (
+            <IconButton onClick={favoriteHandler} size="lg" variant="plain">
+              <FavoriteBorder />
+            </IconButton>
+          )}
         </div>
       </div>
     </>
